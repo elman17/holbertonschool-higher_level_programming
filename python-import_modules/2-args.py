0382eff9c39458e00690f3a3644c8a792a3ece93
elman <elman.muradov17@gmail.com>
2026-05-06 12:30:12 +0400

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    number_of_args = len(argv) - 1
+
+    if number_of_args == 0:
+        print("0 arguments.")
+    elif number_of_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{number_of_args} arguments:")
+
+    i = 1
+    while i < len(argv):
+        print(f"{i}: {argv[i]}")
+        i += 1
