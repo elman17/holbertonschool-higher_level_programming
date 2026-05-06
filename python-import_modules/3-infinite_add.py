f05b856af0cffd9a4e713c7a288ce3f86990d390
elman <elman.muradov17@gmail.com>
2026-05-06 12:46:44 +0400

    3-infinite_add.py

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    result = 0
+
+    i = 1
+    while i < len(argv):
+        result = result + int(argv[i])
+        i += 1
+
+    print(result)
