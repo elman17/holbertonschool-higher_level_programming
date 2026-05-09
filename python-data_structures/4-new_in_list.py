db6a5e028103b7df4847a54b2b37c36ead968ab9
elman <elman.muradov17@gmail.com>
2026-05-09 15:04:39 +0400

    4-new_in_list.py

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -5,7 +5,7 @@ def new_in_list(my_list, idx, element):
     if idx < 0:
         copied_list = my_list.copy()
         return copied_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         copied_list = my_list.copy()
         return copied_list
     else:
