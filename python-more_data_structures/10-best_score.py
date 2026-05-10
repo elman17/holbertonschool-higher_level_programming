0241c5a49aa931c9167a2ba08e12c68e965ba616
elman <elman.muradov17@gmail.com>
2026-05-10 17:59:08 +0400

    10-best_score

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,6 +2,8 @@
 
 
 def best_score(a_dictionary):
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
     max_score = float("-inf")
     best_key = None
     for key, value in a_dictionary.items():
