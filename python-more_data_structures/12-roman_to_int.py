09ae1c7f27c135cdec13b3da3d0357ab1b87f366
elman <elman.muradov17@gmail.com>
2026-05-10 18:24:09 +0400

    12-roman_to_int

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,8 @@
 
 
 def roman_to_int(roman_string):
-    if not roman_string or not isinstance(roman_string, str):
+    if not roman_string or not isinstance(roman_string,
+                                          str):
         return 0
 
     roman_values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
