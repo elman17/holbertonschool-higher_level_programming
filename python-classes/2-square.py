513b85a38f1d4abc48a0cdd47ce2bb4c4349bd83
elman <elman.muradov17@gmail.com>
2026-05-12 12:21:30 +0400

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """Represents a square"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         """Intitializotion square size with validation"""
         if type(size) is not int:
             raise TypeError("size must be an integer")
