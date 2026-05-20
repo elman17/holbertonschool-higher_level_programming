a707ff93d482dd1e4610a8f392ba7f258dc1fa66
elman <elman.muradov17@gmail.com>
2026-05-20 17:55:42 +0400

    4-rectangle.py

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""This module defines a Rectangle class"""
+
+
+class Rectangle:
+    """Defines a rectangle"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("heighht mush be integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return rectangle area"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """Return rectangle perimeter"""
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Return string representation using #."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rows = []
+
+        for i in range(self.__height):
+            rows.append("#" * self.__width)
+        return "\n".join(rows)
