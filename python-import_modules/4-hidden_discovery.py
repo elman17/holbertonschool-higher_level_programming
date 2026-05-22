dc1ef6573cfe42b74865ed6cf9b6ae77bbe62a58
elman <elman.muradov17@gmail.com>
2026-05-23 03:37:05 +0400

    1-my-list.py

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 import hidden_4
+
 print(dir(hidden_4))
--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,4 +6,7 @@ class MyList(list):
     """inherits from list"""
 
     def print_sorted(self):
-        print(sorted(self))
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
+        return new_list
