e2f69ba70e963a419700fe9099b1800246b7c8ee
elman <elman.muradov17@gmail.com>
2026-05-23 03:37:56 +0400

    1-my-list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -6,7 +6,4 @@ class MyList(list):
     """inherits from list"""
 
     def print_sorted(self):
-        new_list = self.copy()
-        new_list.sort()
-        print(new_list)
-        return new_list
+        print(sorted(self))
