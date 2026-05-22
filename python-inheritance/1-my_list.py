911be7eb62a9e09037395e45160ee8f7ccc2700f
elman <elman.muradov17@gmail.com>
2026-05-23 03:23:06 +0400

    README.md

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines MyList class"""
+
+
+class MyList(list):
+    """A class that inherits from list"""
+
+    def print_sorted(self):
+        """Prints the list in ascending order"""
+        print(sorted(self))
