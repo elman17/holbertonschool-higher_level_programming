6b359050af06a78e0101bc8687f9b980801a7d65
elman <elman.muradov17@gmail.com>
2026-05-23 03:35:10 +0400

    1-my-list.py

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""This module defines MyList class"""
+"""MyList module"""
 
 
 class MyList(list):
-    """A class that inherits from list"""
+    """inherits from list"""
 
     def print_sorted(self):
-        """Prints the list in ascending order"""
         print(sorted(self))
--- python-more_classes/python-inheritance/0-lookup.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-
-
-def lookup(obj):
-    dir(obj)
