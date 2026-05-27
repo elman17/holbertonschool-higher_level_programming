00ed8ee9131dfbe8f74bff77c1b332b7a50e2536
elman <elman.muradov17@gmail.com>
2026-05-28 01:52:05 +0400

    9-recangle

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Square"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Square interitance from Rectangle"""
+
+    def __init__(self, size):
+        """Size"""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Area of Square"""
+        return self.__size * self.__size
