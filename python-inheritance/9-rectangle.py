777cdddf531ef591eff345546504928cdbe53621
elman <elman.muradov17@gmail.com>
2026-05-27 23:20:22 +0400

    9-recangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Full Rectangle"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle inheritence from BaseGeometry"""
+
+    def __init__(self, width, heigth):
+        """Width and height"""
+        self.integer_validator("width", width)
+        self.integer_validator("height", heigth)
+
+        self.__width = width
+        self.__height = heigth
+
+    def area(self):
+        """Area of Rectangle"""
+
+        return self.__height * self.__width
+
+    def str(self):
+        "Rectangle area for user"
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
