22126dfd6a014dbfd0c3a9bda35f520c18adaf37
elman <elman.muradov17@gmail.com>
2026-05-28 15:48:27 +0400

    02-verbolist

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Extending the Python List with Notifications"""
+
+
+class VerboseList(self):
+    """A special list class that displays a notification on the screen for each operation"""
+
+    def append(self, item):
+        super().append(item)
+        print("Added [{}] to the list".format(item))
+
+    def extenden(self, item_list):
+        length = len(item_list)
+        super().extend(item_list)
+        print("Extended the list with [{}] item".format(length))
+
+    def remove(self, item):
+        print("Remove [{item}] from the list".format(item))
+        super().remove(item)
+
+    def pop(self, index=-1):
+        item = self[index]
+        print("Popped [{}] from the list".format(item))
+        return super().pop(index)
