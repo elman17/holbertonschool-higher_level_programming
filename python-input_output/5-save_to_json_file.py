aec86b609f542e5d315649effda7b82894ff4f11
elman <elman.muradov17@gmail.com>
2026-05-29 04:10:23 +0400

    5-save

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """Saves object to a file in JSON format"""
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(my_obj, f)
