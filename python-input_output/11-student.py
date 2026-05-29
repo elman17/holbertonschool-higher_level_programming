a32b68d7c0a28981757f276430fb914bf29778a2
elman <elman.muradov17@gmail.com>
2026-05-29 05:17:19 +0400

    11-student

---
+++ python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Student to disk and reload"""
+
+
+class Student:
+    """Student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            result = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    result[key] = value
+            return result
+        return self.__dict__
+    
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value) 
