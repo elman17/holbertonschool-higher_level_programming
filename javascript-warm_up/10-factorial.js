69571d828bc3406205f6b50d6786299f1e33c0a8
elman <elman.muradov17@gmail.com>
2026-06-01 23:59:16 +0400

    10-Js

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
-function name(params) {
-    
+function factorial(n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
+
+console.log(factorial(parseInt(process.argv[2])));
