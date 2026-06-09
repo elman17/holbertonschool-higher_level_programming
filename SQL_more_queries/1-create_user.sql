7330b23a22874b60e0c86a2013f0f5b3ca81a40d
elman <elman.muradov17@gmail.com>
2026-06-09 16:35:45 +0400

    1-SQL

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Root user
 
-CREATE USER IF NOT EXISTS '0_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
