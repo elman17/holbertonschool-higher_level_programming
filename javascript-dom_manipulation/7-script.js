9c7601894fe8b62db42dd9f7f4b9d9f3c709dd79
elman <elman.muradov17@gmail.com>
2026-07-09 15:56:29 +0400

    7-JS

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,15 @@
+const listMovies = document.querySelector("#list_movies");
+const url = "https://swapi-api.hbtn.io/api/films/?format=json";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    data.results.forEach(function (movie) {
+      const newLi = document.createElement("li");
+
+      newLi.textContent = movie.title;
+      listMovies.appendChild(newLi);
+    });
+  });
