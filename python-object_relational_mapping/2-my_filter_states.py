0d8117827f37331b18b99ab027b5345e22e68c43
elman <elman.muradov17@gmail.com>
2026-07-10 02:44:54 +0400

    2-DB

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ def filter_states_by_input():
     
     db = MySQLdb.connect(
         host = "localhost",
-        port = 3306,
+        port =3306,
         user = sys.argv[1],
         passwd = sys.argv[2],
         db = sys.argv[3]
