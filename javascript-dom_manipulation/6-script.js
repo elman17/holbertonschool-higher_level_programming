3572ef94c65ca2a85c9f23a0759bfb265e02180c
elman <elman.muradov17@gmail.com>
2026-07-09 15:44:09 +0400

    6-JS

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+const characterDiv = document.querySelector("#character");
+const url = "https://swapi-api.hbtn.io/api/people/5/?format=json";
+
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    characterDiv.textContent = data.name;
+  });
