4f5f53e25890542d3797366396f5e1da8520ebf0
elman <elman.muradov17@gmail.com>
2026-07-10 02:46:13 +0400

    2-DB

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -4,33 +4,35 @@
 import MySQLdb
 import sys
 
+
 def filter_states_by_input():
     """Connects to the database and fetches states"""
-    
+
     db = MySQLdb.connect(
-        host = "localhost",
-        port =3306,
-        user = sys.argv[1],
-        passwd = sys.argv[2],
-        db = sys.argv[3]
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
     )
-    
+
     cursor = db.cursor()
-    
+
     sql_query = (
-        "SELECT * FROM states WHERE name LIKE BINARY '{}' "
+        "SELECT * FROM states WHERE name LIKE BINARY '{}' " 
         "ORDER BY id ASC"
     ).format(sys.argv[4])
-    
+
     cursor.execute(sql_query)
-    
+
     query_rows = cursor.fetchall()
-    
+
     for row in query_rows:
         print(row)
-    
+
     cursor.close()
     db.close()
+
 
 if __name__ == "__main__":
     filter_states_by_input()
