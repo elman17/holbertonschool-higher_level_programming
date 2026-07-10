7705535ec3115d4ee29d68858b7eb742d566d620
elman <elman.muradov17@gmail.com>
2026-07-10 14:29:35 +0400

    4-DB

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -19,9 +19,9 @@ def list_cities_by_state():
     cursor = db.cursor()
 
     sql_query = (
-        "SELECT cities.id, cities.name, state.name "
+        "SELECT cities.id, cities.name, states.name "
         "FROM cities "
-        "JOIN states ON cities.state_id = state.id "
+        "JOIN states ON cities.state_id = states.id "
         "ORDER BY cities.id ASC"
     )
 
