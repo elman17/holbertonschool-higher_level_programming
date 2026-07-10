0ea16d64a5320614d84c1d2fc5c768a0a2a390c1
elman <elman.muradov17@gmail.com>
2026-07-10 18:13:45 +0400

    6-DB

---
+++ python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""First state model"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represent a state entity for the MtSQL database table state."""
+
+    __tablename__ = "state"
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
