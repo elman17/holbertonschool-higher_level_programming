2137c4160e1cc9b6246e105addca1365604991da
elman <elman.muradov17@gmail.com>
2026-07-11 16:11:55 +0400

    7-DB

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""All states via SQLAlchemy"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_all_states():
+    """Connects to the database and fetches all state objects using ORM"""
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+    engine = create_engine(db_url, pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id.asc()).all()
+
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    fetch_all_states()
