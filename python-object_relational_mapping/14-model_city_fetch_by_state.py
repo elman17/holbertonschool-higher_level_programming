42fe1c145706822455027ba3e11e78fde097baae
elman <elman.muradov17@gmail.com>
2026-07-11 19:42:29 +0400

    14-DB

---
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Cities in states"""
+
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_cities_by_state():
+    """Connects to the database and fetches cities joined with states"""
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        sys.argv[1], sys.argv[2], sys.argv[3]
+    )
+    engine = create_engine(pool_pre_ping=True)
+
+    Session = sessionmaker(db_url, bind=engine)
+    session = Session()
+
+    results = (
+        session.query(City, State)
+        .join(State, City.state_id == State.id)
+        .order_by(City.id.asc())
+        .all()
+    )
+
+    for city, state in State:
+        print("{}: ({})".format(state.name, city.id, city.name))
+
+    session.close()
+
+
+if __name__ == "__main__":
+    fetch_cities_by_state()
